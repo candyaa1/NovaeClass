abf181329c6ac9aaaa93ae0c3f30bfcab7595d68
candyaa1 <candyaaron00@gmail.com>
2026-01-13 22:59:16 -0500

    Update settings.py

--- NovaeClass/settings.py
+++ NovaeClass/settings.py
@@ -29,10 +29,14 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 
 
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS",
-    "127.0.0.1,localhost,novaeclass.onrender.com,www.novaeclass.com"
-).split(",")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "kubernetes.docker.internal",
+    "novaeclass.onrender.com",
+    "www.novaeclass.com",
+]
+
 
 # Application definition
 
