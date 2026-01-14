7126bdc0227c735ac07abad33d2e8bd52066f5c1
candyaa1 <candyaaron00@gmail.com>
2026-01-13 22:32:40 -0500

    Update settings.py

--- NovaeClass/settings.py
+++ NovaeClass/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["www.novaeclass.com", "novaeclass.com", "novaeclass-vhq2.onrender.com", "127.0.0.1"]
+
+
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost,novaeclass.onrender.com,www.novaeclass.com").split(",")
 
 # Application definition
 
