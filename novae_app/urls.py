a344249236873be803c2985a6dc7fffdd9afa02f
candyaa1 <candyaaron00@gmail.com>
2026-01-18 20:50:33 -0500

    Update urls.py

--- novae_app/urls.py
+++ novae_app/urls.py
@@ -11,7 +11,7 @@ from .views import coming_soon
 #
 # Simple redirect view for the homepage
 def home_redirect(request):
-    return redirect('landing')  # Redirect to the signup page
+    return redirect('student_signup')  # Redirect to the signup page
 urlpatterns = [
     # ---------------------------
     # Landing & Logout
